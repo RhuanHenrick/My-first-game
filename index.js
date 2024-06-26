87bbd9da8fcf67df7da4a0d2013b19430dd95149
Rhuan Henrick <pagaoRl@gmail.com>
2024-06-26 16:33:49 -0300

    Alterado as cores do joguinho

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function init() {
     computerNumber = Math.floor(Math.random() *100 + 1)
     console.log(computerNumber)
 }
-function compareNumbers(){
+function compareNumbers() {
     const userNumber = Number(document.getElementById("inputBox").value)
     userNumbers.push(userNumber)
     document.getElementById("guesses").innerHTML=  userNumbers.join("-")
